e2e2dada7a132b822f33a156ab30fdbf3936794e
Jonathan Griggs <jonathan.griggs@gmail.com>
2011-05-30 22:00:03 -0600

    Initial implementation of SpokenDialect to support Speak service

---
+++ src/main/java/com/memetix/mst/SpokenDialect.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright 2011 Jonathan Griggs <jonathan.griggs at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.memetix.mst;
+
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * SpokenLanguage - an enum of all spoken language codes supported by the Microsoft Translator API for the Speak Service
+ * 
+ * @author Jonathan Griggs <jonathan.griggs at gmail.com>
+ */
+public enum SpokenDialect {
+        CATALAN_SPAIN("ca-es"),
+        DANISH_DENMARK("da-dk"),
+        GERMAN_GERMANY("de-de"),
+        ENGLISH_AUSTRALIA("en-au"),
+        ENGLISH_CANADA("en-ca"),
+        ENGLISH_UNITED_KINGDOM("en-uk"),
+        ENGLISH_INDIA("en-in"),
+        ENGLISH_UNITED_STATES("en-us"),
+        SPANISH_SPAIN("es-es"),
+        SPANISH_MEXICO("es-mx"),
+        FINNISH_FINLAND("fi-fi"),
+        FRENCH_CANADA("fr-ca"),
+        FRENCH_FRANCE("fr-fr"),
+        ITALIAN_ITALY("it-it"),
+        JAPANESE_JAPAN("ja-jp"),
+        KOREAN_KOREA("ko-kr"),
+        NORWEGIAN_NORWAY("nb-no"),
+        DUTCH_NETHERLANDS("nl-nl"),
+        POLISH_POLAND("pl-pl"),
+        PORTUGUESE_BRAZIL("pt-br"),
+        PORTUGUESE_PORTUGAL("pt-pt"),
+        RUSSIAN_RUSSIA("ru-ru"),
+        SWEDISH_SWEDEN("sv-se"),
+        CHINESE_SIMPLIFIED_PEOPLES_REPUBLIC_OF_CHINA("zh-cn"),
+        CHINESE_TRADITIONAL_HONG_KONG_SAR("zh-hk"),
+        CHINESE_TRADITIONAL_TAIWAN("zh-tw");
+
+	/**
+	 * Microsoft's String representation of this language.
+	 */
+	private final String language;
+        
+        /**
+         * Internal Localized Name Cache
+         */
+        private Map<Language,String> localizedCache = new ConcurrentHashMap<Language,String>();
+	
+	/**
+	 * Enum constructor.
+	 * @param pLanguage The language identifier.
+	 */
+	private SpokenDialect(final String pLanguage) {
+		language = pLanguage;
+	}
+	
+	public static SpokenDialect fromString(final String pLanguage) {
+		for (SpokenDialect l : values()) {
+			if (l.toString().equals(pLanguage)) {
+				return l;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Returns the String representation of this language.
+	 * @return The String representation of this language.
+	 */
+	@Override
+	public String toString() {
+		return language;
+	}
+        
+        public static void setKey(String pKey) {
+            LanguageService.setKey(pKey);
+        }
+        
+        /**
+         * getName()
+         * 
+	 * Returns the name for this language in the tongue of the specified locale
+         * 
+         * If the name is not cached, then it retrieves the name of ALL languages in this locale.
+         * This is not bad behavior for 2 reasons:
+         * 
+         *      1) We can make a reasonable assumption that the client will request the
+         *         name of another language in the same locale 
+         *      2) The GetLanguageNames service call expects an array and therefore we can 
+         *         retrieve ALL names in the same, single call anyway.
+         * 
+	 * @return The String representation of this language's localized Name.
+	 */
+        public String getName(Language locale) throws Exception {
+            String localizedName = null;
+            if(this.localizedCache.containsKey(locale)) {
+                localizedName = this.localizedCache.get(locale);
+            } else {
+              
+                //If not in the cache, pre-load all the Language names for this locale
+                String[] names = LanguageService.execute(SpokenDialect.values(), locale);
+                int i = 0;
+                for(SpokenDialect lang : SpokenDialect.values()) {
+                    lang.localizedCache.put(locale,names[i]);
+                    i++;
+                }
+                localizedName = this.localizedCache.get(locale);
+            }
+            return localizedName;
+        }     
+        
+        // Flushes the localized name cache for this language
+        public void flushNameCache() {
+            this.localizedCache.clear();
+        }
+        
+        private final static class LanguageService extends MicrosoftAPI {
+            private static final String SERVICE_URL = "http://api.microsofttranslator.com/V2/Ajax.svc/GetLanguageNames?locale=";
+            
+        /**
+         * Detects the language of a supplied String.
+         * 
+         * @param text The String to detect the language of.
+         * @return A DetectResult object containing the language, confidence and reliability.
+         * @throws Exception on error.
+         */
+        public static String[] execute(final SpokenDialect[] targets, final Language locale) throws Exception {
+                String[] localizedNames = new String[0];
+                if(locale==Language.AUTO_DETECT) {
+                    return localizedNames;
+                }
+
+                final String targetString = buildStringArrayParam(SpokenDialect.values());
+
+                final URL url = new URL(SERVICE_URL 
+                        +URLEncoder.encode(locale.toString(), ENCODING)
+                        +"&languageCodes=" + URLEncoder.encode(targetString, ENCODING)
+                        +"&appId="+apiKey);
+                localizedNames = retrieveStringArr(url);
+                return localizedNames;
+        }
+
+     }
+}
---
+++ src/test/java/com/memetix/mst/SpokenDialectTest.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2011 Jonathan Griggs <jonathan.griggs at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.memetix.mst;
+
+import java.net.URL;
+import java.util.Properties;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Jonathan Griggs <jonathan.griggs at gmail.com>
+ */
+public class SpokenDialectTest extends TestCase {
+    Properties p;
+    public SpokenDialectTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        p = new Properties();
+        URL url = ClassLoader.getSystemResource("META-INF/config.properties");
+        p.load(url.openStream());
+        String apiKey = p.getProperty("microsoft.translator.api.key");
+        Language.setKey(apiKey);
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of values method, of class Language.
+     */
+    public void testValues() {
+        SpokenDialect[] expResult = null;
+        SpokenDialect[] result = SpokenDialect.values();
+    }
+
+    /**
+     * Test of valueOf method, of class Language.
+     */
+    public void testValueOf() {
+        String name = "ENGLISH_UNITED_STATES";
+        SpokenDialect expResult = SpokenDialect.ENGLISH_UNITED_STATES;
+        SpokenDialect result = SpokenDialect.valueOf(name);
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of fromString method, of class Language.
+     */
+    public void testFromString() {
+        String pLanguage = "en-us";
+        SpokenDialect expResult = SpokenDialect.ENGLISH_UNITED_STATES;
+        SpokenDialect result = SpokenDialect.fromString(pLanguage);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of toString method, of class Language.
+     */
+    public void testToString() {
+        SpokenDialect instance = SpokenDialect.ENGLISH_UNITED_STATES;
+        String expResult = "en-us";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+    }
+
+    
+    /**
+     * Test of getLanguageName method, of class Language.
+     */
+    public void testGetNameLocalized() throws Exception {
+        Language locale = Language.ENGLISH;
+        String expResult = "French (Canada)";
+        String result = SpokenDialect.FRENCH_CANADA.getName(locale);
+        assertEquals(expResult, result);
+        
+        locale = Language.FRENCH;
+        expResult = "Anglais (Inde)";
+        result = SpokenDialect.ENGLISH_INDIA.getName(locale);
+        assertEquals(expResult, result);
+    }
+    
+    public void testGetAllNamesLocalizedCached() throws Exception {
+        String name;
+        //Flush the caches, so we can test for timing
+        for(Language lang : Language.values()) {
+            lang.flushNameCache();
+        }
+        
+        long startTime1 = System.currentTimeMillis();
+        for(Language lang : Language.values()) {
+            name = lang.getName(Language.FRENCH);
+            //System.out.println(name + " : " + lang.toString());
+        }
+        long totalTime1 = System.currentTimeMillis()-startTime1;
+        
+        long startTime2 = System.currentTimeMillis();
+        for(Language lang : Language.values()) {
+            name = lang.getName(Language.FRENCH);
+            //System.out.println(name + " : " + lang.toString());
+        }
+        long totalTime2 = System.currentTimeMillis()-startTime2;
+        //System.out.println("Uncached: " + totalTime1 + "ms, Cached: " + totalTime2 + "ms");
+        assert totalTime1 > totalTime2;
+        
+        /* Uncomment this block to eyeball and make sure the name localization is working for all languages
+        for(Language lang : Language.values()) {
+            System.out.println(lang.toString() + " / " + Language.VIETNAMESE.getName(lang));
+        }
+        */
+    }
+}
