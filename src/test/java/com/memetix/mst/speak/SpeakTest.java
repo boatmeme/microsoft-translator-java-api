8f79dfeda40d2f26b020356953985438b0780508
Jonathan Griggs <jonathan.griggs@gmail.com>
2011-06-02 11:20:41 -0600

    Changed copyright name

--- src/test/java/com/memetix/mst/speak/SpeakTest.java
+++ src/test/java/com/memetix/mst/speak/SpeakTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 boatmeme.
+ * Copyright 2011 Jonathan Griggs.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
